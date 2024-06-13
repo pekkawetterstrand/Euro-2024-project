00d502634932334ec0e15b6f3f62a3cf9c9a5b56
pekkawetterstrand <pekka.wetterstrand@gmail.com>
2024-06-13 11:13:47 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,5 @@
 import { updateGroupTable } from './groupTable.js';
 
-// Remaining code stays the same...
-
 const games = [
     { team1: 'Germany', team2: 'France', date: '2024-06-14', group: 'A' },
     { team1: 'Italy', team2: 'Spain', date: '2024-06-15', group: 'A' },
