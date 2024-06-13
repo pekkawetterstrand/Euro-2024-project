5e3056f1ba2c747a77365801619336433c596688
pekkawetterstrand <pekka.wetterstrand@gmail.com>
2024-06-13 12:36:32 +0300

    Update registration.js

--- scripts/registration.js
+++ scripts/registration.js
@@ -1,26 +1,17 @@
 export function handleRegistration() {
     const registrationForm = document.getElementById('registration-form');
-    const registrationError = document.getElementById('registration-error');
-    const registrationPage = document.getElementById('registration-page');
-    const gamePage = document.getElementById('game-page');
 
     registrationForm.addEventListener('submit', (event) => {
         event.preventDefault();
         const nickname = document.getElementById('nickname').value.trim();
         if (nickname) {
             localStorage.setItem('nickname', nickname);
-            registrationPage.classList.add('hidden');
-            gamePage.classList.remove('hidden');
+            document.getElementById('registration-page').classList.add('hidden');
+            document.getElementById('game-page').classList.remove('hidden');
         } else {
-            registrationError.textContent = 'Please enter a valid name or nickname.';
-            registrationError.style.display = 'block';
+            alert('Please enter a nickname.');
         }
     });
-
-    const storedNickname = localStorage.getItem('nickname');
-    if (storedNickname) {
-        registrationPage.classList.add('hidden');
-        gamePage.classList.remove('hidden');
-    }
 }
+
 
